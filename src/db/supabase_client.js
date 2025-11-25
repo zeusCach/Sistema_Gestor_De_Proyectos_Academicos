6cd5ee191a7918b193a7282caa4f19dc58d0bd03
zeusCach <zeus.cach@outlook.com>
2025-11-25 09:53:01 -0500

    Creacion de varibles de conexion a supabase

---
+++ src/db/supabase_client.js
@@ -0,0 +1,9 @@
+import {createCliente } from "@supabase/supabase-js";
+
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY 
+
+export const supabase = createCliente(supabaseUrl, supabaseUrl);
+

b456f382c056c10cbce0b8a842a2a51cc0711f81
zeusCach <zeus.cach@outlook.com>
2025-11-25 17:02:38 -0500

    Creando nueva ruta para dashboard

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,7 +3,7 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginForm } from "../components/LoginForm.jsx";
 import { RegisterForm } from "../components/RegisterForm.jsx";
-
+import { Dashboard} from "../components/Dashboard.jsx";
 
 export const AppRouter = () => {
     return (
@@ -13,6 +13,7 @@ export const AppRouter = () => {
 
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm/>}></Route>
+            <Route path="/dashboard" element={<Dashboard/>}></Route>
         </Routes>
     )
 }

3d242d4294c2a2a562689a7a86cbcae7b2bacd2c
zeusCach <zeus.cach@outlook.com>
2025-11-25 10:31:42 -0500

    Comentando funcionalidad

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,18 +1,24 @@
 import { supabase } from "../db/supabase_client"
 
 //funcion asincrona que registrara usuarios publicadores
-const registerUser = async ({ name, email, password, institution, phone_number }) => {
+export const registerUser = async ({ name, email, password, institution, phone_number }) => {
 
+    //crea autenticacion con supabase por medio de el email y el password
     const { data: authData, error: authError } = await supabase.auth.singUp({
         email: email,
         password: password
     });
 
+    //si existe un error en autenticacion de usuario falla dando error
     if (authError) return { error: authError };
 
+    //Obtiene el usuario ID, verifica si existe en authData
     const userId = authData.user?.id;
+
+    //Si no existe nos devuelve el error
     if (!userId) return { error: "No se pudo obtener el ID del usuario." };
 
+    //Insertamos los datos capturados en el form a nuestra tabla users
     const { error: dbError } = await supabase.from("users").insert([{
         user_id: userId,
         name,
@@ -22,7 +28,9 @@ const registerUser = async ({ name, email, password, institution, phone_number }
         user_type_id: 1
     }]);
 
+    // si existe un error en la insercion de los datos, devuelve el error
     if(dbError) return {error: errorDb}
 
+    //succes: true, si los datos se insertan correctamente
     return {success: true}
 }

a9a9adfee599ee4728ee955377e0086547c3f136
zeusCach <zeus.cach@outlook.com>
2025-11-26 00:07:24 -0500

    Asignando Proteccion al dashboard para que solo los logueados tengan acceso

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,7 +3,7 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginForm } from "../components/LoginForm.jsx";
 import { RegisterForm } from "../components/RegisterForm.jsx";
-import { Dashboard} from "../components/Dashboard.jsx";
+import { Dashboard } from "../components/Dashboard.jsx";
 
 export const AppRouter = () => {
     return (
@@ -12,8 +12,16 @@ export const AppRouter = () => {
             <Route path="/" element={<LoginForm />} />
 
             <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm/>}></Route>
-            <Route path="/dashboard" element={<Dashboard/>}></Route>
+            <Route path="/register" element={<RegisterForm />}></Route>
+
+            {/* Protejemos el dashboard con ProtectedRoute para que solo
+             usuarios logueados pueden entrar al dashboard
+             */}
+            <Route path="/dashboard" element={
+                <ProtectedRoute>
+                    <Dashboard />
+                </ProtectedRoute>
+            }></Route>
         </Routes>
     )
 }

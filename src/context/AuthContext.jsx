aeb9145f2bf12a0c14f58e444f534ef7872a9aa7
zeusCach <zeus.cach@outlook.com>
2025-11-26 00:13:37 -0500

    Importando librerias

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import supabase from "../db/supabase_client";
 
 
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import { LoginForm } from "../components/LoginForm.jsx";
 import { RegisterForm } from "../components/RegisterForm.jsx";
 import { Dashboard } from "../components/Dashboard.jsx";
+import { ProtectedRoute } from "./ProtectedRoute.jsx";
 
 export const AppRouter = () => {
     return (

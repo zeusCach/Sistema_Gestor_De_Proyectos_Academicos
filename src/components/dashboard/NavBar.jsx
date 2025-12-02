77e44fa3a6589a58510d85a1eb1d9c7871a93327
zeusCach <zeus.cach@outlook.com>
2025-12-02 01:37:37 -0500

    Implementando component notificaciones

--- src/components/dashboard/NavBar.jsx
+++ src/components/dashboard/NavBar.jsx
@@ -4,6 +4,7 @@ import { getProfilePhoto } from "../../services/profileService";
 import Logo_isc from "../../assets/Logo_isc.png"; // CORRECTO
 import logo_TecNM from "../../assets/Logo_TecNM.png"; // CORRECTO
 import logo_educacion from "../../assets/logo_educacion.png"; // CORRECTO
+import { NotificationsDropdown } from "../notifications/NotificationsDropdown";
 
 export const Navbar = ({ userName = "Zeus" }) => {
 
@@ -79,12 +80,7 @@ export const Navbar = ({ userName = "Zeus" }) => {
             </div>
 
             {/* Notificaciones */}
-            <button className="relative p-2 text-gray-600 hover:text-gray-900">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-              </svg>
-              <span className="absolute top-1 right-1 block h-2 w-2 rounded-full bg-red-500"></span>
-            </button>
+            <NotificationsDropdown />
 
             {/* Avatar y nombre */}
             <div className="flex items-center space-x-3">
